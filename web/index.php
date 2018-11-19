e6ff404dc0b9ab585124d92818867f0721f3103e
cesar9022 <44823577+cesar9022@users.noreply.github.com>
2018-11-19 17:48:54 -0500

    CAMBIO PARA CONEXION

--- web/index.php
+++ web/index.php
@@ -22,4 +22,14 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+$app->get('/guardar/{conteo}', 
+	function($conteo) use($app){
+	$dbconexion=pg_connect( "host=ec2-23-23-80-20.compute-1.amazonaws.com port=5432 dbname=d7l7vav8kt18d9 user=oabpbkgthojhbk password=090bf4ff331f1181a615fdaa44038b861a20f8532c2612444b432002add70ebf");
+	$registro=array (
+		"FECHA"=>date('Y-m-d H:i:s'),
+		"BOTELLASLLENAS"=>$conteo);
+	$resultado=pg_insert ($dbconexion,'VARIABLES',$registro);
+	return date('Y-m-d H:i:s');
+	});
+
 $app->run();
