4f3be49515b10dd3439fa18de9234451f695de22
cesar9022 <44823577+cesar9022@users.noreply.github.com>
2018-11-20 20:39:49 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -36,7 +36,8 @@ $app->get('/consultarConteo',
 	function() use($app){
 	$dbconexion=pg_connect( "host=ec2-23-23-80-20.compute-1.amazonaws.com port=5432 dbname=d7l7vav8kt18d9 user=oabpbkgthojhbk password=090bf4ff331f1181a615fdaa44038b861a20f8532c2612444b432002add70ebf");
 	$resultado=pg_query ($dbconexion,'SELECT "BOTELLASLLENAS" FROM public."VARIABLES" ORDER BY "FECHA" DESC LIMIT 1');
-	return pg_fetch_all($resultado);
+	$conteo = pg_fetch_all($resultado);
+		return $conteo[0];
 	});
 
 $app->run();
